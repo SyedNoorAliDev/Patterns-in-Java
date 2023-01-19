9691aa224cecd65590127b1c7f7674e6a89bdb79
Syed Noor Ali <96229280+SyedNoorAliDev@users.noreply.github.com>
2023-01-19 18:47:55 +0500

    Add files via upload

---
+++ right Angled Triangle.java
@@ -0,0 +1,12 @@
+public class RightAngledTriangle{
+// This is for printing right angled triangle
+    public static void main(String args[]){
+        int n = 6;
+        for (int i=1; i<=n;i++){
+            for (int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
